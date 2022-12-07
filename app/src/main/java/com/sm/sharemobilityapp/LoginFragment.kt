0181535485fbad89ad27bdf421e0c1d2cd5c8111
Bart Frijters <bart.frijters@gmail.com>
2022-12-07 14:23:38 +0100

    Navigation between 2 fragments using databinding...
    Steps to add fragments to navigation functions:
    1. Add fragments to nav_graph.xml (Easy with new destination button in Design view)
    2. Access the element needed for navigation (buttons...) within the Fragment's onViewCreated function with a reference to the next Fragment it needs to navigate to. (See HomeFragment.kt or LoginFragment.kt

---
+++ app/src/main/java/com/sm/sharemobilityapp/LoginFragment.kt
@@ -0,0 +1,52 @@
+package com.sm.sharemobilityapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.findNavController
+import com.sm.sharemobilityapp.databinding.FragmentLoginBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [LoginFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class LoginFragment : Fragment() {
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+
+
+    override fun onCreate(savedInstanceState : Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.homeButton.setOnClickListener { view ->
+            view.findNavController().navigate(R.id.homeFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
