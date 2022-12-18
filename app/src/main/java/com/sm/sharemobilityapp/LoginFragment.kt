2702d9a9c60abdb6a65b3558a0787f97de7e7df6
Bart Frijters <bart.frijters@gmail.com>
2022-12-18 20:21:44 +0100

    -> merge of XMLWireFrames into Navigation
    + navigational functionality to the nav drawer menu (home, profile, login, logout)
    + navigational functionality to login button on the login fragment
    + temporary navigational functionality to 'filters button' on start fragment for testing purposes.
    
    - home fragment used as testing in previous version of branch.

--- app/src/main/java/com/sm/sharemobilityapp/LoginFragment.kt
+++ app/src/main/java/com/sm/sharemobilityapp/LoginFragment.kt
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil.setContentView
 import androidx.navigation.findNavController
 import com.sm.sharemobilityapp.databinding.FragmentLoginBinding
 
