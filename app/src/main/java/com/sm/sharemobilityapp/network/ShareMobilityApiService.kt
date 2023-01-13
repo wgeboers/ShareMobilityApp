971d30a82520d89220e29f0bc3f15d406a85b9b4
MarcelForman <91687704+MarcelForman@users.noreply.github.com>
2023-01-13 10:00:29 +0100

    Removed double import

--- app/src/main/java/com/sm/sharemobilityapp/network/ShareMobilityApiService.kt
+++ app/src/main/java/com/sm/sharemobilityapp/network/ShareMobilityApiService.kt
@@ -4,7 +4,6 @@ import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import okhttp3.ResponseBody
 import retrofit2.Response
-import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.*
