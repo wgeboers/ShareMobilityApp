ca8505d5e6b5161b74c6bb0002292289cc502063
MarcelForman <91687704+MarcelForman@users.noreply.github.com>
2023-01-16 15:55:51 +0100

    Added Unit test for UserDao
    Works if it fails

---
+++ app/src/androidTest/java/com/sm/sharemobilityapp/UserDaoTest.kt
@@ -0,0 +1,88 @@
+package com.sm.sharemobilityapp
+
+import android.util.Log
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SmallTest
+import com.sm.sharemobilityapp.data.SMRoomDatabase
+import com.sm.sharemobilityapp.data.user.User
+import com.sm.sharemobilityapp.data.user.UserDao
+import junit.framework.Assert.*
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.collect
+import okhttp3.internal.wait
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.util.concurrent.CountDownLatch
+
+//@RunWith(AndroidJUnit4::class)
+//@SmallTest
+class UserDaoTest {
+    private lateinit var database: SMRoomDatabase
+    private lateinit var userDao: UserDao
+
+    @Before
+    fun setupDatabase() {
+        database = Room.inMemoryDatabaseBuilder(
+            ApplicationProvider.getApplicationContext(),
+            SMRoomDatabase::class.java
+        ).allowMainThreadQueries().build()
+        userDao = database.userDao
+    }
+
+    @After
+    fun closeDatabase() {
+        database.close()
+    }
+    @Test
+    fun insertUserTest() = runBlocking {
+        val user1 = User (
+        -1,
+        "CAR_USER",
+        "George2",
+        "letmein",
+        "welkom",
+                "Piet",
+            "Straat 23 2986CP",
+            0)
+        userDao.insert(user1)
+
+       // var users: List<User>
+
+       var users = launch { userDao.getUsers().collect() {user ->
+           Log.d("User collect", user.toString())
+           user.forEach {
+               assert("George" == it.username)
+               Log.d("User collect", it.username)
+           }
+       }
+
+       }
+
+
+
+       // Log.d("Test", users.toString())
+       // user.collect()
+      //  Log.d("User collect", user.toString())
+
+
+
+//        val latch = CountDownLatch(1)
+//        var result: List<User>
+//        val job = async(Dispatchers.IO) {
+//           userDao.getUsers().collect() { user ->
+//               user.forEach() {
+//                   Log.d("forEach", user[0].firstname)
+//                   assertEquals("Timo", user[0].firstname)
+//               }
+//
+//            }
+//        }
+//
+//        latch.await()
+//        job.cancelAndJoin()
+    }
+}
