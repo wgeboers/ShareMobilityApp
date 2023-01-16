b7cdcbf6837bae51ead19be5896fd82f6f75c6c8
MarcelForman <91687704+MarcelForman@users.noreply.github.com>
2023-01-16 21:51:04 +0100

    UserDaoTest
    Refactor

--- app/src/androidTest/java/com/sm/sharemobilityapp/UserDaoTest.kt
+++ app/src/androidTest/java/com/sm/sharemobilityapp/UserDaoTest.kt
@@ -8,18 +8,14 @@ import androidx.test.filters.SmallTest
 import com.sm.sharemobilityapp.data.SMRoomDatabase
 import com.sm.sharemobilityapp.data.user.User
 import com.sm.sharemobilityapp.data.user.UserDao
-import junit.framework.Assert.*
 import kotlinx.coroutines.*
-import kotlinx.coroutines.flow.collect
-import okhttp3.internal.wait
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import java.util.concurrent.CountDownLatch
 
-//@RunWith(AndroidJUnit4::class)
-//@SmallTest
+@RunWith(AndroidJUnit4::class)
+@SmallTest
 class UserDaoTest {
     private lateinit var database: SMRoomDatabase
     private lateinit var userDao: UserDao
@@ -58,33 +54,10 @@ class UserDaoTest {
            user.forEach {
                assert("George" == it.username)
                Log.d("User collect", it.username)
-               this.coroutineContext.cancel()
+             this.coroutineContext.cancel()
            }
        }
 
        }
-
-
-
-       // Log.d("Test", users.toString())
-       // user.collect()
-      //  Log.d("User collect", user.toString())
-
-
-
-//        val latch = CountDownLatch(1)
-//        var result: List<User>
-//        val job = async(Dispatchers.IO) {
-//           userDao.getUsers().collect() { user ->
-//               user.forEach() {
-//                   Log.d("forEach", user[0].firstname)
-//                   assertEquals("Timo", user[0].firstname)
-//               }
-//
-//            }
-//        }
-//
-//        latch.await()
-//        job.cancelAndJoin()
     }
 }
